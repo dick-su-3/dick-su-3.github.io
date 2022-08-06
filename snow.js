f1e8bc213ebf6f93cd3ca2b16766669e50989f4a
suguru <18505949578@139.com>
2022-08-06 19:58:50 +0800

    Site updated: 2022-08-06 19:58:48

--- snow.js
+++ snow.js
@@ -1,71 +1,48 @@
-var snow = {
-  info: {
-    top: 0,
-    left: 0,
-    zIndex: 500,
-    number: 70
-  },
-  down: function () {
-    var f = window.innerWidth;
-    var g = window.innerHeight;
-    var d = document.createElement("canvas");
-    d.style.position = "fixed";
-    d.style.pointerEvents = "none";
-    d.style.top = snow.info.top + "px";
-    d.style.left = snow.info.left + "px";
-    d.style.zIndex = snow.info.zIndex;
-    d.width = f;
-    d.height = g;
-    document.body.appendChild(d);
-    var c = [];
-    for (var a = 0; a < snow.info.number; a++) {
-      c.push({
-        x: Math.random() * f,
-        y: Math.random() * g,
-        r: Math.random() * 4 + 1,
-        n: Math.random() * 70
-      })
-    }
-    var b = d.getContext("2d");
-    var e = 0;
-    setInterval(function () {
-        b.clearRect(0, 0, f, g);
-        b.fillStyle = "rgba(255, 255, 255, 0.6)";
-        b.shadowBlur = 5;
-        b.shadowColor = "rgba(255, 255, 255, 0.9)";
-        b.beginPath();
-        for (var j = 0; j < 70; j++) {
-          var h = c[j];
-          b.moveTo(h.x, h.y);
-          b.arc(h.x, h.y, h.r, 0, Math.PI * 2, 0)
-        }
-        b.fill();
-        e += 0.01;
-        for (var j = 0; j < 70; j++) {
-          var h = c[j];
-          h.y += Math.cos(e + h.n) + h.r / 2;
-          h.x += Math.sin(e) * 2;
-          if (h.x > f + 5 || h.x < -5 || h.y > g) {
-            c[j] = j % 3 > 0 ? {
-              x: Math.random() * f,
-              y: -10,
-              r: h.r,
-              n: h.n
-            } : Math.sin(e) > 0 ? {
-              x: -5,
-              y: Math.random() * g,
-              r: h.r,
-              n: h.n
-            } : {
-              x: f + 5,
-              y: Math.random() * g,
-              r: h.r,
-              n: h.n
-            }
-          }
-        }
-      },
-      15)
-  }
-};
-// yilia-plus主题已经集成了，可以直接用
+/*样式一*/
+(function ($) {
+    $.fn.snow = function (options) {
+        var $flake = $('<div id="snowbox" />').css({
+                'position': 'absolute',
+                'z-index': '9999',
+                'top': '-50px'
+            }).html('❄'),
+            documentHeight = $(document).height(),
+            documentWidth = $(document).width(),
+            defaults = {
+                minSize: 10,
+                maxSize: 20,
+                newOn: 1000,
+                flakeColor: "#AFDAEF" /* 此处可以定义雪花颜色，若要白色可以改为#FFFFFF */
+            },
+            options = $.extend({}, defaults, options);
+        var interval = setInterval(function () {
+            var startPositionLeft = Math.random() * documentWidth - 100,
+                startOpacity = 0.5 + Math.random(),
+                sizeFlake = options.minSize + Math.random() * options.maxSize,
+                endPositionTop = documentHeight - 200,
+                endPositionLeft = startPositionLeft - 500 + Math.random() * 500,
+                durationFall = documentHeight * 10 + Math.random() * 5000;
+            $flake.clone().appendTo('body').css({
+                left: startPositionLeft,
+                opacity: startOpacity,
+                'font-size': sizeFlake,
+                color: options.flakeColor
+            }).animate({
+                top: endPositionTop,
+                left: endPositionLeft,
+                opacity: 0.2
+            }, durationFall, 'linear', function () {
+                $(this).remove()
+            });
+        }, options.newOn);
+    };
+})(jQuery);
+$(function () {
+    $.fn.snow({
+        minSize: 5,
+        /* 定义雪花最小尺寸 */
+        maxSize: 45,
+        /* 定义雪花最大尺寸 */
+        newOn: 300 /* 定义密集程度，数字越小越密集 */
+    });
+});
